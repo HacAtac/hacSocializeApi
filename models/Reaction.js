f22c92d8cddee7bfdd0c0b21f014153c25e52852
HacAtac <jhack00@icloud.com>
2021-12-03 00:01:00 -0600

    create ReactionSchema imported/exported whats needed

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Types.ObjectId,
+      default: new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      min: 1,
+      mac: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = ReactionSchema;
