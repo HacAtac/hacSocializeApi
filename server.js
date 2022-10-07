fe5a96b2ae5ff74cdadd59148e43af4952605395
HacAtac <jhackworth00@outlook.com>
2022-10-07 13:02:02 -0500

    update mongoDB connection added few packages

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,18 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require("./routes"));
 
+dotenv.config();
+connectDB();
+
 //connect to mongoose db
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/hac-db",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, //use this to avoid deprecation warning when using mongoose 5.x and above
-  }
-);
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true, //use this to avoid deprecation warning when using mongoose 5.x and above
+// });
 
 // Use this to log mongo queries being executed
 mongoose.set("debug", true);
 
 app.listen(PORT, () =>
-  console.log(`HacAtac Connected on localhost:${PORT} <(^_^)>`)
+  console.log(`HacAtac Connected on localhost:${PORT} <(^_^)>`.brightGreen)
 );
